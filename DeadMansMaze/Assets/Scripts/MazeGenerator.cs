64a854f1ec5f4281483b4bf802ea7a039db6ca75
kaiteur1967 <rwoon@hotmail.com>
2023-07-07 01:25:33 -0400

    Initiate Maze Generator Prototype
    Initiate the generation of the maze for prototyping using primitive cubes

---
+++ DeadMansMaze/Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCoordinate
+{
+    public int x, z;
+    
+    public MapCoordinate(int _x, int _z)
+    {
+        x = _x;
+        z = _z;
+    }
+}
+
+public class MazeGenerator : MonoBehaviour
+{
+    private List<MapCoordinate> directions = new List<MapCoordinate>()
+    {
+        new MapCoordinate(1, 0),
+        new MapCoordinate(0, 1),
+        new MapCoordinate(-1, 0),
+        new MapCoordinate(0, -1)
+    };
+
+    [SerializeField]
+    private int _width = 20;    // x axis
+    [SerializeField]
+    private int _depth = 20;    // z axis
+    [SerializeField]
+    private GameObject _mazeContainer;
+
+    private byte[,] _map;
+    private int _scale = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeMaze();
+        GenerateMaze();
+    }
+
+    private void InitializeMaze()
+    {
+        // define size of map array
+        _map = new byte[_width, _depth];
+
+        // 1 = wall, 0 = tunnel
+        // Initialize maze to all walls and no tunnels
+        for (int x = 0; x < _width; x++)
+        {
+            for (int z = 0; z < _depth; z++)
+            {
+                _map[x, z] = 1;
+            }
+        }
+    }
+
+    private void GenerateMaze()
+    {
+        for (int z = 0; z < _depth; z++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                if (_map[x, z] == 1)
+                {
+                    Vector3 pos = new Vector3(x * _scale, 0, z * _scale);
+                    GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    wall.transform.localScale = new Vector3(_scale, _scale, _scale);
+                    wall.transform.parent = _mazeContainer.transform;
+                    wall.transform.position = pos;
+                }
+            }
+        }
+    }
+}
