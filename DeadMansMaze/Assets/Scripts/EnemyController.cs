e40a6b1fb1f3b6614bbf87c0b0f56b71a7057465
Ernesto <hkarakaki@gmail.com>
2023-08-21 00:34:51 +0300

    Added new files for the enemies prefabs, added EnemyController script

---
+++ DeadMansMaze/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Animator anim;
+    float xMove = 1f;
+    float yMove = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //HideCursor();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        anim.SetBool("isMoving", true);
+
+        anim.SetFloat("x", xMove, 0.25f, Time.deltaTime);
+        anim.SetFloat("y", yMove, 0.25f, Time.deltaTime);
+    }
+}
