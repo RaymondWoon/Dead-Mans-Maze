6dec394a744de19e154a46692ecd3c365cef4af2
Alwin Wong YC <67592526+Alwinwon@users.noreply.github.com>
2023-09-12 19:59:52 +0800

    Added 3rd Person Perspective & Optimized Player Animation and Controls
    Added 3rd Person perspective toggle with "spacebar". Also, tweaked Player Movement Animation & Controls including bug fixes of main camera clipping though objects and weapons.

---
+++ DeadMansMaze/Assets/Player/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class is not a MonoBehavior as it will live as a property of the player.
+public class Inventory
+{
+    // Declare class variables
+    private List<string> keyItems =  new List<string>();
+
+
+    public bool HasItem(string k)
+    {
+        return keyItems.Contains(k.ToLower());
+    }
+
+    public void AddItem(string k)
+    {
+        keyItems.Add(k.ToLower());
+    }
+
+    public int GetNumberOfKeyItem(string k)
+    {
+        int count = 0;
+        foreach (string item in keyItems) {
+            if (item.ToLower() == k.ToLower()) count++;
+        }
+        return count;
+    }
+}
--- DeadMansMaze/Assets/Player/Scripts/PlayerAim.cs
+++ DeadMansMaze/Assets/Player/Scripts/PlayerAim.cs
@@ -5,36 +5,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerAim : MonoBehaviour
 {
-  Camera cam;
-  Ray mouseRay;
+  // Shot 
+  public ParticleSystem AimHit;
+
+  // Shoot
   Vector3 hitPoint;
-  Vector3 currentPoint;
+  Vector3 hitDirection;
+  bool Surface = false;
 
   // Start is called before the first frame update
   void Start()
   {
-    cam = Camera.main;
+
   }
 
   // Update is called once per frame
   void Update()
   {
     // Raycast from middle of the screen
+    Camera cam = Camera.main;
     Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-    mouseRay = cam.ScreenPointToRay(screenCenterPoint);
+    Ray mouseRay = cam.ScreenPointToRay(screenCenterPoint);
 
     // Raycast to aim at colliders or maximum distance from origin
     RaycastHit hitInfo;
-    if(Physics.Raycast(mouseRay, out hitInfo, 10f))
+    if (Physics.Raycast(mouseRay, out hitInfo, 10f))
     {
       hitPoint = hitInfo.point;
+      hitDirection = hitInfo.normal;
+      Surface = true;
     }
     else
     {
       hitPoint = mouseRay.origin + mouseRay.direction * 10f;
+      Surface = false;
     }
 
     // Transform position to raycasted destination
     transform.position = hitPoint;
+  }
+
+  public void HitEffect()
+  {
+    if (Surface)
+    {
+      AimHit.transform.position = hitPoint;
+      AimHit.transform.forward = hitDirection;
+      AimHit.Emit(1);
+    }
   }
 }
