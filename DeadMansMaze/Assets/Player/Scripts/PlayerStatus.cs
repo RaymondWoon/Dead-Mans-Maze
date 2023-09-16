704b9b0de9fd05725e7f306d403dc37151d1fce5
kaiteur1967 <rwoon@hotmail.com>
2023-09-16 03:31:56 -0400

    Update
    Minor updates for latest build

--- DeadMansMaze/Assets/Player/Scripts/PlayerStatus.cs
+++ DeadMansMaze/Assets/Player/Scripts/PlayerStatus.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerStatus
 {
     //[SerializeField]
-    public static int currentHp = 50;
+    public static int currentHp = 100;
     public static int maxHp = 100;
     [SerializeField]
     public Inventory inventory = new Inventory();
