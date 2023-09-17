82df2a752b77b58144db5a506146001e8874b2b4
kaiteur1967 <rwoon@hotmail.com>
2023-09-17 04:22:46 -0400

    Countdown timer
    Implemented a countdown timer to indicate the time to complete the game

---
+++ DeadMansMaze/Assets/Scripts/Game Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Text _textTime;
+
+    // Variables
+    private float _timeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Time to complete is seconds
+        _timeRemaining = (float)MainManager.Instance.TimeToComplete * 60.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timeRemaining > 0)
+        {
+            _timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            _timeRemaining = 0.0f;
+        }
+
+        DisplayTime(_timeRemaining);
+    }
+
+    // Display in minutes and secconds
+    private void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0.0f)
+            timeToDisplay = 0;
+
+        int minutes = (int)timeToDisplay / 60;
+        int seconds = (int)timeToDisplay % 60;
+
+        _textTime.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
